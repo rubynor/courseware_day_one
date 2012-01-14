9d84719ccb5df0399fdfbdd84cc8a8c63ebd01e4
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2012-01-14 19:56:34 +0100

    Basic syntax slides done

--- agenda_day_one.mm
+++ agenda_day_one.mm
@@ -1,28 +1,28 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1325663427135" ID="ID_1040894105" MODIFIED="1326490816737" TEXT="roughing out">
-<node CREATED="1325663526738" ID="ID_1733691548" MODIFIED="1326554442708" POSITION="right" TEXT="Regex slide">
-<font NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1326554431152" ID="ID_833556920" MODIFIED="1326554446925" POSITION="right" TEXT="Exception handling slide"/>
-<node CREATED="1326554461784" ID="ID_251692461" MODIFIED="1326554465111" POSITION="right" TEXT="Class and object slides"/>
-<node CREATED="1326554465368" ID="ID_280053458" MODIFIED="1326554467141" POSITION="right" TEXT="Module slides"/>
-<node CREATED="1326554472752" ID="ID_1527201127" MODIFIED="1326554475926" POSITION="right" TEXT="Metaprogramming slides"/>
-<node CREATED="1326554499728" ID="ID_252523607" MODIFIED="1326557089111" POSITION="right" TEXT="Basic Syntax exercises">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1326557089681" ID="ID_1057839857" MODIFIED="1326557095234" POSITION="right" TEXT="OOP Exercises">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1326492623902" ID="ID_271601889" MODIFIED="1326554524038" POSITION="left" TEXT="watch well-grounded nuby talk, another pass">
+<node CREATED="1325663427135" ID="ID_1040894105" MODIFIED="1326563641197" TEXT="finish day one">
+<node CREATED="1326563596711" ID="ID_496810372" MODIFIED="1326563627117" POSITION="right" TEXT="Basic syntax section">
+<node CREATED="1326557089681" ID="ID_1057839857" MODIFIED="1326563609288" TEXT="Exercises">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1326492163741" ID="ID_881022777" MODIFIED="1326492634536" POSITION="left" TEXT="do a &quot;idiom&quot; pass">
+</node>
+<node CREATED="1326563590583" ID="ID_1363626043" MODIFIED="1326563593836" POSITION="right" TEXT="OOP section">
+<node CREATED="1326554461784" ID="ID_251692461" MODIFIED="1326554465111" TEXT="Class and object slides"/>
+<node CREATED="1326554465368" ID="ID_280053458" MODIFIED="1326554467141" TEXT="Module slides"/>
+<node CREATED="1326554472752" ID="ID_1527201127" MODIFIED="1326554475926" TEXT="Metaprogramming slides"/>
+<node CREATED="1326554499728" ID="ID_252523607" MODIFIED="1326563609288" TEXT="Exercises">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1326492623902" ID="ID_271601889" MODIFIED="1326567061709" POSITION="right" TEXT="watch well-grounded nuby talk, another pass">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#ff0000" CREATED="1326492163741" ID="ID_881022777" MODIFIED="1326567061709" POSITION="right" TEXT="do a &quot;idiom&quot; pass">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1326493283216" ID="ID_1340464856" MODIFIED="1326493285767" TEXT="eloquent ruby"/>
 <node CREATED="1326493286008" ID="ID_1410622440" MODIFIED="1326493288617" TEXT="ruby best practices"/>
 </node>
-<node CREATED="1326493250026" ID="ID_788928566" MODIFIED="1326493268481" POSITION="left" TEXT="brush up metaprogramming and read the ruby prog language, then final pass">
+<node COLOR="#ff0000" CREATED="1326493250026" ID="ID_788928566" MODIFIED="1326567061709" POSITION="right" TEXT="brush up metaprogramming and read the ruby prog language, then final pass">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
