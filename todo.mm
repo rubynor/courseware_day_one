81e2cfdd83967c52cc25f84f17f41790bb78469e
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2012-01-15 18:20:24 +0100

    Lesser exercise scope

--- todo.mm
+++ todo.mm
@@ -2,22 +2,24 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1325663427135" ID="ID_1040894105" MODIFIED="1326563641197" TEXT="finish day one">
 <node CREATED="1326563590583" ID="ID_1363626043" MODIFIED="1326568745462" POSITION="right" TEXT="OOP section">
-<node CREATED="1326554472752" ID="ID_1527201127" MODIFIED="1326554475926" TEXT="Metaprogramming slides"/>
-<node CREATED="1326582624060" ID="ID_1386632763" MODIFIED="1326582627180" TEXT="Exercises">
+<node CREATED="1326582624060" ID="ID_1386632763" MODIFIED="1326647942902" TEXT="ex 11">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1326647943201" ID="ID_992337529" MODIFIED="1326647950317" TEXT="ex 12">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1326647944737" ID="ID_725302754" MODIFIED="1326647948777" TEXT="ex 13">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1326576136254" ID="ID_118082892" MODIFIED="1326582934397" POSITION="right" TEXT="Gem section">
-<node CREATED="1326582636411" ID="ID_492943398" MODIFIED="1326582640392" TEXT="Slides"/>
-<node CREATED="1326582640707" ID="ID_198624724" MODIFIED="1326627787948" TEXT="Exercise">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff0000" CREATED="1326492623902" ID="ID_271601889" MODIFIED="1326629399791" POSITION="right" TEXT="watch well-grounded nuby talk, another pass">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
+<node COLOR="#ff0000" CREATED="1326629279394" ID="ID_853461250" MODIFIED="1326629399791" POSITION="right" TEXT="read The Ruby language, another pass">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#ff0000" CREATED="1326492623902" ID="ID_271601889" MODIFIED="1326567061709" POSITION="right" TEXT="watch well-grounded nuby talk, another pass">
-<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1326493250026" ID="ID_788928566" MODIFIED="1326582963905" POSITION="right" TEXT="brush up metaprog and The Ruby Language, do final tweaks">
-<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff0000" CREATED="1326493250026" ID="ID_788928566" MODIFIED="1326629399791" POSITION="right" TEXT="brush up on metaprogramming and class/object stuff, tweak further">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </map>
