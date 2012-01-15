d420f1056505b4f494521f0c591a3931c3542da4
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2012-01-15 12:58:33 +0100

    more work

---
+++ todo.mm
@@ -0,0 +1,23 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1325663427135" ID="ID_1040894105" MODIFIED="1326563641197" TEXT="finish day one">
+<node CREATED="1326563590583" ID="ID_1363626043" MODIFIED="1326568745462" POSITION="right" TEXT="OOP section">
+<node CREATED="1326554472752" ID="ID_1527201127" MODIFIED="1326554475926" TEXT="Metaprogramming slides"/>
+<node CREATED="1326582624060" ID="ID_1386632763" MODIFIED="1326582627180" TEXT="Exercises">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1326576136254" ID="ID_118082892" MODIFIED="1326582934397" POSITION="right" TEXT="Gem section">
+<node CREATED="1326582636411" ID="ID_492943398" MODIFIED="1326582640392" TEXT="Slides"/>
+<node CREATED="1326582640707" ID="ID_198624724" MODIFIED="1326627787948" TEXT="Exercise">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1326492623902" ID="ID_271601889" MODIFIED="1326567061709" POSITION="right" TEXT="watch well-grounded nuby talk, another pass">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#ff0000" CREATED="1326493250026" ID="ID_788928566" MODIFIED="1326582963905" POSITION="right" TEXT="brush up metaprog and The Ruby Language, do final tweaks">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
