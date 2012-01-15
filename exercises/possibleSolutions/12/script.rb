d962fe08c74b11286d434058bc2c8d4a7863af60
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2012-01-15 19:35:55 +0100

    Finished ex 12

--- exercises/possibleSolutions/12/script.rb
+++ exercises/possibleSolutions/12/script.rb
@@ -1,39 +1,42 @@
-class Tree
-  include Enumerable
-  attr_accessor :children, :node_value
+module RubyKurs
 
-  def initialize(value = nil)
-    @node_value = value
-    @children = []
-  end
-  
-  def add(child)
-    @children.push child
-  end
-  
-  def each
-    traverse_down { |node| yield(node) }
-  end
+  class Tree
+    include Enumerable
+    attr_accessor :children, :node_value
 
-  # Run given block on current node and all its descendants recursively
-  def traverse_down(&block)
-    block.call(self)
-    if(!children.nil?)
-      children.each{ |child| child.traverse_down(&block) }
+    def initialize(value = nil)
+      @node_value = value
+      @children = []
     end
-  end
+    
+    def add(child)
+      @children.push child
+    end
+    
+    def each
+      traverse_down { |node| yield(node) }
+    end
 
-  def to_s(indentation=0)
-    node_descr = ""
-    indentation.times { node_descr.concat "  " }
-    node_descr.concat "#{@node_value}\n"    
-    if (!children.nil?)
-      children.each do |child| 
-        node_descr.concat("#{child.to_s(indentation+1)}")
+    # Run given block on current node and all its descendants recursively
+    def traverse_down(&block)
+      block.call(self)
+      if(!children.nil?)
+        children.each{ |child| child.traverse_down(&block) }
       end
     end
-    return node_descr
-  end
- 
-end
 
+    def to_s(indentation=0)
+      node_descr = ""
+      indentation.times { node_descr.concat "  " }
+      node_descr.concat "#{@node_value}\n"    
+      if (!children.nil?)
+        children.each do |child| 
+          node_descr.concat("#{child.to_s(indentation+1)}")
+        end
+      end
+      return node_descr
+    end
+    
+  end
+
+end
--- exercises/possibleSolutions/12/test.rb
+++ exercises/possibleSolutions/12/test.rb
@@ -2,11 +2,11 @@ require "./script.rb"
 require "test/unit"
 
 class TestTheScript < Test::Unit::TestCase
-
-   def test_tree_structure
-    parent = Tree.new "parent"
-    child = Tree.new "child1"
-    second_child = Tree.new "child2"
+ 
+  def test_tree_structure
+    parent = RubyKurs::Tree.new "parent"
+    child = RubyKurs::Tree.new "child1"
+    second_child = RubyKurs::Tree.new "child2"
     parent.add child
     parent.add second_child
     assert_equal(child, parent.children[0])
@@ -14,9 +14,9 @@ class TestTheScript < Test::Unit::TestCase
   end
 
   def single_parent_two_children
-    parent = Tree.new
-    child = Tree.new
-    second_child = Tree.new
+    parent = RubyKurs::Tree.new "parent"
+    child = RubyKurs::Tree.new "child1"
+    second_child = RubyKurs::Tree.new "child2"
     parent.add child
     parent.add second_child
     return parent
@@ -32,8 +32,10 @@ class TestTheScript < Test::Unit::TestCase
   def test_enumerable_mixin
     tree = single_parent_two_children    
     assert_equal 3, tree.count
-    assert_equal 3, (tree.count { |node| node.class == Tree })
-    assert_equal ["Tree", "Tree", "Tree"], tree.map { |node| node.class.to_s }
+    assert_equal 3, (tree.count { |node| node.class == RubyKurs::Tree })
+    assert_equal ["RubyKurs::Tree", "RubyKurs::Tree", "RubyKurs::Tree"], tree.map { |node| node.class.to_s }
+    # TODO find_all
+    # TODO any?
   end
 
   def test_tostring
@@ -43,7 +45,9 @@ parent
   child1
   child2
 AS_STRING
-    assert_equal expected, single_parent_two_children.to_s
+    actual = single_parent_two_children.to_s
+    puts actual
+    assert_equal expected, actual
   end
   
   
